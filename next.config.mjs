2f9768acb80a319292e084865213d3eb69b8ff13
Renato Craveiro <51494560+renato-craveiro@users.noreply.github.com>
2026-01-21 14:15:28 +0000

    Add output configuration and disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ import createMDX from '@next/mdx';
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  output: 'export',
+  // Optional: Change the output directory `out` to something else
+  // distDir: 'dist',
+  
+  // Disable server-based image optimization
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
